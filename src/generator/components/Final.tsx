3e2e46dc4c7461c8032845e4e6ec62b7a878b04b
Odin Kammerloher <kammerloher.andreas@gmail.com>
2023-05-19 19:04:36 +0200

    Add explanations to "Final"

--- src/generator/components/Final.tsx
+++ src/generator/components/Final.tsx
@@ -1,11 +1,13 @@
-import { faFileExport, faFloppyDisk, faTrash } from "@fortawesome/free-solid-svg-icons"
+import { faCheck, faFileExport, faFloppyDisk, faTrash } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { Button, Stack } from "@mantine/core"
+import { Alert, Button, Stack, Text } from "@mantine/core"
 import { Character } from "../../data/Character"
 import { downloadCharacterSheet } from "../pdfCreator"
 import { downloadJson } from "../utils"
 import { useDisclosure } from "@mantine/hooks"
 import ResetModal from "../../components/ResetModal"
+import { IconAlertCircle } from "@tabler/icons-react"
+import { faCheckCircle, faCheckSquare } from "@fortawesome/free-regular-svg-icons"
 
 
 type FinalProps = {
@@ -18,15 +20,24 @@ const Final = ({ character, setCharacter, setSelectedStep }: FinalProps) => {
     const [resetModalOpened, { open: openResetModal, close: closeResetModal }] = useDisclosure(false)
 
     return (
-        <div>
+        <div style={{ maxWidth: "440px" }}>
+            <Text ta={"center"} fz={"50px"}><FontAwesomeIcon icon={faCheckSquare} color="green" /></Text>
             <h1>Character Creation Complete</h1>
+
+            <div style={{ background: "rgba(0, 0, 0, 0.6)" }}>
+                <Text fz={"xl"} mb={"xl"}>You can now export to a printable PDF or download your character as JSON file, which you can later load again to continue editing</Text>
+            </div>
             <Stack align="center" spacing="xl">
 
                 <Button leftIcon={<FontAwesomeIcon icon={faFileExport} />} size="xl" color="grape" onClick={() => { downloadCharacterSheet(character) }}>Download PDF</Button>
-                <Button leftIcon={<FontAwesomeIcon icon={faFloppyDisk} />} size="xl" color="yellow" variant="light" onClick={() => { downloadJson(character) }}>Download JSON</Button>
+                <Button leftIcon={<FontAwesomeIcon icon={faFloppyDisk} />} size="lg" color="yellow" variant="light" onClick={() => { downloadJson(character) }}>Download JSON</Button>
                 <Button leftIcon={<FontAwesomeIcon icon={faTrash} />} size="md" color="red" variant="subtle" onClick={() => { openResetModal() }}>Reset</Button>
 
             </Stack>
+
+            <Alert mt={"50px"} icon={<IconAlertCircle size="1rem" />} color="violet" variant="outline" bg={"rgba(0, 0, 0, 0.6)"}>
+                <Text fz={"lg"} ta={"center"}>You may need to refresh your browser to trigger multiple downloads!</Text>
+            </Alert>
 
             <ResetModal setCharacter={setCharacter} setSelectedStep={setSelectedStep} resetModalOpened={resetModalOpened} closeResetModal={closeResetModal} />
         </div>
