3a31f2394fbe92dbf073b2099c3b964864e3a0dc
Odin Kammerloher <kammerloher.andreas@gmail.com>
2023-06-28 22:09:37 +0200

    Increase "smallScreen" width - 1300 is still small for Progeny

--- src/globals.tsx
+++ src/globals.tsx
@@ -4,7 +4,7 @@ export const globals = {
     viewportWidthPx: 800,
     isPhoneScreen: false,
     isSmallScreen: false,
-    smallScreenW: 800,
+    smallScreenW: 1300,
     phoneScreenW: 440,
     tooltipTriggerEvents: { hover: true, focus: true, touch: true },  // TODO: How to make tooltips work for phone long-press..?
 
