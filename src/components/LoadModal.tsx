62c56211aab04772b5f29654c761c1dcf6f1f280
Odin Kammerloher <kammerloher.andreas@gmail.com>
2023-09-29 00:17:10 +0200

    Add (mostly) meaningful error message when loading character fails

--- src/components/LoadModal.tsx
+++ src/components/LoadModal.tsx
@@ -4,6 +4,8 @@ import { Button, Divider, Group, Modal, Stack, Text } from "@mantine/core"
 import { Buffer } from "buffer"
 import { Character, characterSchema } from "../data/Character"
 import { getUploadFile } from "../generator/utils"
+import { notifications } from "@mantine/notifications"
+import { z } from "zod"
 
 export type LoadModalProps = {
     setCharacter: (character: Character) => void
@@ -32,17 +34,29 @@ const LoadModal = ({ loadModalOpened, closeLoadModal, setCharacter, loadedFile }
                                 console.log("Error: No file loaded!")
                                 return
                             }
-                            const fileData = await getUploadFile(loadedFile)
-                            const base64 = fileData.split(",")[1]
-                            const json = Buffer.from(base64, "base64").toString()
-                            const parsed = JSON.parse(json)
-                            console.log({ loadedCharacter: parsed })
+                            try {
+                                const fileData = await getUploadFile(loadedFile)
+                                const base64 = fileData.split(",")[1]
+                                const json = Buffer.from(base64, "base64").toString()
+                                const parsed = JSON.parse(json)
+                                console.log({ loadedCharacter: parsed })
 
-                            if (!parsed["rituals"]) parsed["rituals"] = [] // backwards compatibility for characters that were saved before rituals were added
-                            if (!parsed["predatorType"]["pickedMeritsAndFlaws"]) parsed["predatorType"]["pickedMeritsAndFlaws"] = [] // backwards compatibility for characters that were saved before pickedMeritsAndFlaws were added
+                                if (!parsed["rituals"]) parsed["rituals"] = [] // backwards compatibility for characters that were saved before rituals were added
+                                if (!parsed["predatorType"]["pickedMeritsAndFlaws"]) parsed["predatorType"]["pickedMeritsAndFlaws"] = [] // backwards compatibility for characters that were saved before pickedMeritsAndFlaws were added
 
-                            setCharacter(characterSchema.parse(parsed))
-                            closeLoadModal()
+                                setCharacter(characterSchema.parse(parsed))
+                                closeLoadModal()
+                            } catch (e) {
+                                if (e instanceof z.ZodError) {
+                                    notifications.show({
+                                        title: "JSON content error loading character",
+                                        message: JSON.stringify(e.formErrors.fieldErrors),
+                                        color: "red",
+                                        autoClose: false,
+                                    })
+                                }
+                                console.log({ e })
+                            }
                         }}
                     >
                         Load/Overwrite character
