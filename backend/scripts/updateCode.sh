d4281a22e32b28a2d772467bd06092c7e058fa21
Odin Kammerloher <kammerloher.andreas@gmail.com>
2026-01-06 14:37:47 +0100

    Update updateCode for increased convenience

--- backend/scripts/updateCode.sh
+++ backend/scripts/updateCode.sh
@@ -5,9 +5,15 @@ cp database.sqlite database.sqlite.backup.$(date +%Y%m%d_%H%M%S)
 echo "Backed up database.sqlite to database.sqlite.backup.$(date +%Y%m%d_%H%M%S)"
 git pull
 echo "Pulled latest code from git"
+npm install
+echo "Installed dependencies"
 npm run build
 echo "Built the code"
 npm run db:migrate
 echo "Migrated the database"
 pm2 restart progeny-backend
 echo "Restarted the backend"
+echo "Waiting 2 seconds for backend to start..."
+sleep 2
+curl https://api.progeny.odin-matthias.de/health
+echo ""
